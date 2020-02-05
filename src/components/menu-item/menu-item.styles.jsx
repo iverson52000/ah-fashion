8adca87e1060fdbfba562630a58b74eebfde9208
Albert Hsu <iverson52000@gmail.com>
2020-02-04 19:26:37 -0800

    fix homepage and signin

--- src/components/menu-item/menu-item.styles.jsx
+++ src/components/menu-item/menu-item.styles.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const MenuItemContainer = styled.div`
   // height: ${({ size }) => (size ? '360px' : '240px')}
-  height: 240px;
+  height: 360px;
   min-width: 30%;
   overflow: hidden;
   flex: 1 1 auto;
